475ea1fed2503d7a5ee77378a8da86afb9e0e0c1
iqra <fhsaleem8104@gmail.com>
2024-12-12 11:56:34 +0500

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    domains: ["images.unsplash.com"], // Allow Unsplash images
+  },
   /* config options here */
 };
 
 export default nextConfig;
+
